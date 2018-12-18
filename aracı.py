f93732fbcd34d258a3ee40dafe6c5c2b91cb7b93
beyzacankus <beyzacankus@gmail.com>
2018-12-18 23:08:00 +0300

    dict.txt de eksik bilgiler var ve HELO cevabı parametrrelerine tekrar bakılmalı

--- aracı.py
+++ aracı.py
@@ -24,10 +24,9 @@ class loggerThread(threading.Thread):
 
 #  Peer'in client tarafi tanimlaniyor.
 class clientThread(threading.Thread):
-    def __init__(self, clientq, serverq, logq, c_uuid):
+    def __init__(self, clientq, logq, c_uuid):
         threading.Thread.__init__(self)
         self.clientq = clientq
-        self.serverq = serverq
         self.logq = logq
         self.c_uuid = c_uuid
 
@@ -37,8 +36,10 @@ class clientThread(threading.Thread):
         s = socket.socket()
         s.connect((host, port))
         type = "A"
-        # Kullanıcı bilgileri datanın içine yazılıyor
+
+        # Kullanici bilgileri datanin içine yaziliyor
         data = str(self.c_uuid) + " " + host + " " + str(port) + " " + type
+
         # Client icin hem reader hem de writer thread calisir
         reader = clientReader(self.logq, self.clientq, s, data)
         sender = clientSender(self.clientq, s)
@@ -47,55 +48,53 @@ class clientThread(threading.Thread):
         sender.start()
 
 
-# Client için sender thread
+# Client icin sender thread
 class clientSender(threading.Thread):
-    def __init__(self, clientq, s):
+    def __init__(self, clientq, soket):
         threading.Thread.__init__(self)
         self.clientq = clientq
-        self.s = s
+        self.soket = soket
 
     def run(self):
-        print("Sender Thread Active")
+        print("Client Sender Thread Active")
         while True:
-
-            # Client queue'den alınanlar servera yollanıyor
+            # Client queue'den alinanlar diger peer'in serverina yollaniyor
             while not self.clientq.empty():
                 data = self.clientq.get()
                 data = data.strip("\n")
-                self.s.send(data.encode())
+                self.soket.send(data.encode())
 
 
-# Client için reader thread
+# Client icin reader thread
 class clientReader(threading.Thread):
-    def __init__(self, logq, clientq, s, data):
+    def __init__(self, logq, clientq, soket, data):
         threading.Thread.__init__(self)
         self.logq = logq
         self.clientq = clientq
-        self.s = s
+        self.soket = soket
         self.data = data
 
     def run(self):
-        print("Reader Thread Active")
-        # Alınan input Client queue'ye koyuluyor
+        print("Client Reader Thread Active")
+        # Alinan input Client queue'ye koyuluyor
         while True:
             inp = input()
             inp = inp.strip("\n")
-            # Eğer "HELO" mesajıysa kullanıcı bilgileri eklenip yollanıyor
+            # Eger "HELO" mesajiysa kullanici bilgileri eklenip yollaniyor
             if inp[:4] == "HELO":
                 data = inp[:4] + " " + self.data + inp[4:len(self.data)]
                 self.clientq.put(data)
             else:
                 self.clientq.put(inp)
             time.sleep(2)
-            print(self.s.recv(1024).decode())
+            print(self.soket.recv(1024).decode())
 
 
-# Server için thread
+# Server icin thread
 class serverThread(threading.Thread):
-    def __init__(self, serverq, clientq, logq, soket, dict):
+    def __init__(self, serverq, logq, soket, dict):
         threading.Thread.__init__(self)
         self.serverq = serverq
-        self.clientq = clientq
         self.logq = logq
         self.soket = soket
         self.dict = dict
@@ -104,19 +103,19 @@ class serverThread(threading.Thread):
         while True:
             c, addr = self.soket.accept()
             print('Got connection from', addr)
-            # Kullanıcı kayıt olduğu zaman flag=1 olur
-            # Eğer kayıt olmadan LIST komutu verilirse AUTH hatası
+            # Kullanici kayit oldugu zaman flag=1 olur
+            # Eger kayit olmadan LIST komutu verilirse AUTH hatasi
             flag = 0
             while True:
                 rps = c.recv(1024).decode()
                 if rps[:4] == "HELO":
                     c_uuid = rps[5:19]
-                    # Eğer önceden kayıtlıysa "WLCM" cevabı veriliyor
+                    # Eger onceden kayitliysa "WLCM" cevabi veriliyor
                     if c_uuid in self.dict:
                         send = "WLCM " + c_uuid
                         c.send(send.encode())
                         flag = 1
-                    # Kayıtlı değilse yeni kayıt oluşturup WAIT cevabı veriliyor
+                    # Kayitli degilse yeni kayit olusturup WAIT cevabi veriliyor
                     else:
                         self.dict[c_uuid] = rps[20:len(rps)]
                         data = c_uuid + " -" + rps[19:len(rps)]
@@ -126,14 +125,14 @@ class serverThread(threading.Thread):
                         flag = 1
                     c.send('\nThank you for connecting!\n'.encode())
 
-                # Dictionary'deki kayıtlar yollanıyor
+                # Dictionary'deki kayitlar yollaniyor
                 elif rps[:4] == "LIST":
                     if flag == 1:
                         c.send(str(self.dict).encode())
                     else:
                         c.send("AUTH".encode())
 
-                # Bağlantı testi
+                # Baglanti testi
                 elif rps[:4] == "SUID":
                     rps = "AUID "
                     c.send(rps.encode())
@@ -144,33 +143,32 @@ class serverThread(threading.Thread):
 
 def main():
 
-    # Server için soket bağlantıları
+    # Server icin soket baglantilari
     s1 = socket.socket()
     host = "0.0.0.0"
     port = 12345
     s1.bind((host, port))
     s1.listen(5)
 
-    # Kullanıcı kayıtlarının tutulacağı dictionary
-    # write_dictionary ile text dosyası çağırılıp önceki kayıtlar dictionary içerisine yazılıyor
+    # Kullanici kayitlarinin tutulacagi dictionary
+    # write_dictionary ile text dosyasi çagirilip onceki kayitlar dictionary icerisine yaziliyor
     server_dict = {}
     write_dictionary(server_dict)
 
     # MAC adresiyle UUID
     client_uuid = uuid.getnode()
-    queueLock = threading.Lock()
     logQueue = queue.Queue()
     ServerQueue = queue.Queue()
     ClientQueue = queue.Queue()
     threads = []
 
-    server_thread = serverThread(ServerQueue, ClientQueue, logQueue, s1, server_dict)
+    server_thread = serverThread(ServerQueue, logQueue, s1, server_dict)
     server_thread.start()
-    client_thread = clientThread(ClientQueue, ServerQueue, logQueue, client_uuid)
+    client_thread = clientThread(ClientQueue, logQueue, client_uuid)
     client_thread.start()
 
 
-# text dosyasındaki kayıtlar dictionary'e yazılıyor.UUID key değeri, geri kalan bilgiler(ip,port,tip,nick) valuelar
+# text dosyasındaki kayitlar dictionary'e yazıliyor. UUID key degeri, geri kalan bilgiler(ip,port,tip,nick) valuelar
 def write_dictionary(server_dict):
     fid = open("dictionary.txt", "r+")
     for line in fid:
@@ -179,7 +177,7 @@ def write_dictionary(server_dict):
             server_dict[listedline[0].strip()] = listedline[1].strip()
     fid.close()
 
-# yeni kaydı text dosyasına ekleme
+# yeni kaydi text dosyasina ekleme
 def append_dictionary(data):
     f = open("dictionary.txt", "a+")
     f.write("%s" % data)
