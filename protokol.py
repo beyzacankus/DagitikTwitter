12484f3cd84165179c743c468661287fc7ef25f5
Emre TORAMAN <emretoraman@outlook.com>
2018-12-26 12:19:45 +0300

    Yeni protokol komutları ve yayinci/araci kontrolu eklendi

--- protokol.py
+++ protokol.py
@@ -10,6 +10,16 @@ list = "LIST"
 listo = "LISTO"
 pubkeygeldi = "PUBR"
 pubkeygitsin = "PUBO"
+pubkeycontrol = "PUBC"
+pubkeyonay = "PUBV"
+pubkeydenied = "PUBD"
+microblogistek = "MBLR"
+microblogokey = "MBLO"
+aboneol = "SUBS"
+aboneoldun = "SUBO"
+aboneliktencik = "UNSB"
+aboneliktenciktin = "UNSO"
+
 
 #types, parser fonksiyonunu çağıran aracı mı ? yayıncı mı ? bunu bilmemiz gerekiyor.
 araci = "araci"
@@ -33,7 +43,6 @@ def parser(data, type): #AUTH ve BLCK hataları ana kod içerisinde yazılacak
             "ctype": splitLine[4].strip(),
             "cnick": splitLine[5].strip()
         }
-        return rdict
     elif(command == suid): #suid argüman almaz ancak auid dönüş yaparken uuid parametresi ile döner.
         rdict = {
             "status": "OK",
@@ -41,8 +50,7 @@ def parser(data, type): #AUTH ve BLCK hataları ana kod içerisinde yazılacak
             "resp": auid
         }
 
-        return rdict
-    elif(command == list):
+    elif(command == list): #liste ve count dönüyoruz.
         rdict = {
             "status": "OK",
             "cmd": list,
@@ -50,8 +58,7 @@ def parser(data, type): #AUTH ve BLCK hataları ana kod içerisinde yazılacak
             "count": splitLine[1].strip()
         }
 
-        return rdict
-    elif(command == pubkeygeldi):
+    elif(command == pubkeygeldi): #cpubkey ile gelen pubkey i alıyoruz. gönderim yaparken kendi pubkey imizi göndereceğiz.
         if(type == yayinci):
             rdict = {
                 "status" : "OK",
@@ -65,7 +72,65 @@ def parser(data, type): #AUTH ve BLCK hataları ana kod içerisinde yazılacak
                 "cmd": pubkeygeldi
             }
 
-        return rdict
+    elif(command == pubkeycontrol): #signed ve text in doğruluğunu kontrol edip resp1 veya resp2 döneceğiz
+        if(type == yayinci):
+            rdict = {
+                "status" : "OK",
+                "cmd": pubkeycontrol,
+                "resp1": pubkeyonay,
+                "resp2": pubkeydenied,
+                "ctext": splitLine[1].strip(),
+                "csigned": splitLine[2].strip()
+            }
+        else:
+            rdict = {
+                "status" : "NOK",
+                "cmd": pubkeycontrol
+            }
+
+    elif(command == microblogistek): #mikroblog onay mesajı ve count dönüyoruz.
+        if(type == yayinci):
+            rdict = {
+                "status" : "OK",
+                "cmd": microblogistek,
+                "resp": microblogokey,
+                "count": splitLine[1].strip()
+            }
+        else:
+            rdict = {
+                "status": "NOK",
+                "cmd": microblogistek
+            }
+    elif(command == aboneol): #abone olma işlemleri
+        if(type == yayinci):
+            rdict = {
+                "status" : "OK",
+                "cmd": aboneol,
+                "resp": aboneoldun
+            }
+        else:
+            rdict = {
+                "status": "NOK",
+                "cmd": aboneol
+            }
+    elif(command == aboneliktencik): #abonelikten cikma islemleri
+        if(type == yayinci):
+            rdict = {
+                "status" : "OK",
+                "cmd": aboneliktencik,
+                "resp": aboneliktenciktin
+            }
+        else:
+            rdict = {
+                "status": "NOK",
+                "cmd": aboneliktencik
+            }
+
+
+
+    return rdict
+
+
 
 
 
