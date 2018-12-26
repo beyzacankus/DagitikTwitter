1e120f5771b91eadbaf96f87d15ea5ee9aff0525
yavuzBahceci <myavuzbahceci@gmail.com>
2018-12-27 01:32:25 +0300

    UI da kullanılacak bazı fonksiyonların taslakları hazırlandı

--- ui/dagitik_twitter_main.py
+++ ui/dagitik_twitter_main.py
@@ -1,7 +1,11 @@
 from PyQt5 import QtWidgets, QtGui, QtCore
 import sys
+import negotiator
+import yayıncı
+import util_functions
+import protokol
 
-from dagitik_proje_ui import Ui_MainWindow
+from ui.dagitik_twitter_ui import Ui_MainWindow
 
 
 class Test_Ui(QtWidgets.QMainWindow):
@@ -12,7 +16,59 @@ class Test_Ui(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        #Connect buton fonksiyonu
+        self.ui.pushButton.pressed.connect(self.connect_to_twitter)
+        self.ui.pushButton_2.pressed.connect(self.log_out)
+        self.ui.pushButton_3.pressed.connect(self.unsubscribe)
+        self.ui.pushButton_6.pressed.connect(self.block_user)
 
+    def connect_to_twitter(self):
+        # girilen nickname ' i al
+        user_nickname = self.ui.plainTextEdit.toPlainText()
+
+        # Yayıncı peer oluştur
+
+        # Yayıncı peer aracı peer'a bağlanacak
+
+        # Yapılmadıysa kayıt işlemleri yapılacak
+
+        # Feed çekilecek
+
+        # Followerlar çekilecek
+
+        # Followedlar çekilecek
+
+        # Blockedler Çekilecek
+
+        # suggestionlar çekilecek
+
+        # Inbox Çekilecek
+
+        # My shares çekilecek
+
+        # Profile:name yazılacak
+
+    def log_out(self):
+        # tüm edit alanları silinecek
+        pass
+
+    def unsubscribe(self):
+
+        #takip edilen seçili isimler alınacak
+
+        #takipten çıkarılacak
+
+        pass
+
+    def block_user(self):
+
+        #seçili user alınacak
+
+        #Blocklanacak
+
+        #Blocked yenilenecek
+
+        pass
 
 
     def run(self):
