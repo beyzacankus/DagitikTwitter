a0e89349f00c8af933f6e3eda2dc29634d30279f
Emre TORAMAN <emretoraman@outlook.com>
2019-01-09 16:24:18 +0300

    AUTH kontrolu

--- protokol.py
+++ protokol.py
@@ -39,126 +39,127 @@ genelhata = "ERRG"
 authhata = "AUTH"
 engelhatasi = "BLCK"
 
-
-#types, parser fonksiyonunu çağıran aracı mı ? yayıncı mı ? bunu bilmemiz gerekiyor.
+# types, parser fonksiyonunu çağıran aracı mı ? yayıncı mı ? bunu bilmemiz gerekiyor.
 araci = "A"
 yayinci = "Y"
 
 
-def parser(data, type): #AUTH ve BLCK hataları ana kod içerisinde yazılacak
+def parser(data, type):  # AUTH ve BLCK hataları ana kod içerisinde yazılacak
     delimiter = " "
     splitLine = data.split(delimiter)
-    command = splitLine[0].strip()
+    command = splitLine[ 0 ].strip()
     try:
 
-        if(command == merhaba):
+        if (command == merhaba):
             rdict = {
                 "status": "OK",
                 "cmd": merhaba,
                 "resp1": bekle,
                 "resp2": hosgeldin,
-                "cuuid": splitLine[1].strip(),
-                "cip": splitLine[2].strip(),
-                "cport": splitLine[3].strip(),
-                "ctype": splitLine[4].strip(),
-                "cnick": splitLine[5].strip(),
+                "cuuid": splitLine[ 1 ].strip(),
+                "cip": splitLine[ 2 ].strip(),
+                "cport": splitLine[ 3 ].strip(),
+                "ctype": splitLine[ 4 ].strip(),
+                "cnick": splitLine[ 5 ].strip(),
             }
-        elif(command == hosgeldin):
+        elif (command == hosgeldin):
             rdict = {
                 'status': "OK",
                 'cmd': hosgeldin,
                 'resp': list
             }
-        elif(command == bekle):
+        elif (command == bekle):
             rdict = {
-                'status':"OK",
-                'cmd':command,
+                'status': "OK",
+                'cmd': command,
             }
-        elif(command == suid): #suid argüman almaz ancak auid dönüş yaparken uuid parametresi ile döner.
+        elif (command == suid):  # suid argüman almaz ancak auid dönüş yaparken uuid parametresi ile döner.
             rdict = {
                 "status": "OK",
                 "cmd": suid,
                 "resp": auid
             }
-        elif(command == auid): #suid argüman almaz ancak auid dönüş yaparken uuid parametresi ile döner.
+        elif (command == auid):  # suid argüman almaz ancak auid dönüş yaparken uuid parametresi ile döner.
             rdict = {
                 "status": "OK",
                 "cmd": auid,
-                "uuid": splitLine[1].strip()
+                "uuid": splitLine[ 1 ].strip()
             }
 
-        elif(command == list): #liste ve count dönüyoruz.
+        elif (command == list):  # liste ve count dönüyoruz.
             rdict = {
                 "status": "OK",
                 "cmd": list,
                 "resp": listo,
             }
-        elif(command == listo):
+        elif (command == listo):
             rdict = {
                 "status": "OK",
-                "cmd":listo,
-                "list":data[5:]
+                "cmd": listo,
+                "list": data[ 5: ]
             }
 
-        elif(command == pubkeygeldi): #cpubkey ile gelen pubkey i alıyoruz. gönderim yaparken kendi pubkey imizi göndereceğiz.
-            if(type == yayinci):
+        elif (
+                command == pubkeygeldi):  # cpubkey ile gelen pubkey i alıyoruz. gönderim yaparken kendi pubkey imizi göndereceğiz.
+            if (type == yayinci):
                 rdict = {
-                    "status" : "OK",
+                    "status": "OK",
                     "cmd": pubkeygeldi,
                     "resp": pubkeygitsin,
-                    "cpubkey": data[5:]
+                    "cpubkey": data[ 5: ]
                 }
             else:
                 rdict = {
-                    "status" : "NOK",
+                    "status": "NOK",
                     "cmd": pubkeygeldi
                 }
-        elif(command == pubkeygitsin): #cpubkey ile gelen pubkey i alıyoruz. gönderim yaparken kendi pubkey imizi göndereceğiz.
-            if(type == yayinci):
+        elif (
+                command == pubkeygitsin):  # cpubkey ile gelen pubkey i alıyoruz. gönderim yaparken kendi pubkey imizi göndereceğiz.
+            if (type == yayinci):
                 rdict = {
-                    "status" : "OK",
+                    "status": "OK",
                     "cmd": pubkeygitsin,
-                    "spubkey": data[5:]
+                    "spubkey": data[ 5: ]
                 }
             else:
                 rdict = {
-                    "status" : "NOK",
+                    "status": "NOK",
                     "cmd": pubkeygitsin
                 }
 
-        elif(command == pubkeycontrol): #signed ve text in doğruluğunu kontrol edip resp1 veya resp2 döneceğiz
-            if(type == yayinci):
+        elif (command == pubkeycontrol):  # signed ve text in doğruluğunu kontrol edip resp1 veya resp2 döneceğiz
+            if (type == yayinci):
                 rdict = {
-                    "status" : "OK",
+                    "status": "OK",
                     "cmd": pubkeycontrol,
                     "resp1": pubkeyonay,
                     "resp2": pubkeydenied,
-                    "ctext": splitLine[1].strip(),
-                    "csigned": splitLine[2].strip()
+                    "ctext": splitLine[ 1 ].strip(),
+                    "csigned": splitLine[ 2 ].strip()
                 }
             else:
                 rdict = {
-                    "status" : "NOK",
+                    "status": "NOK",
                     "cmd": pubkeycontrol
                 }
 
-        elif(command == microblogistek): #mikroblog onay mesajı ve count dönüyoruz.
-            if(type == yayinci):
+        elif (command == microblogistek):  # mikroblog onay mesajı ve count dönüyoruz.
+            if (type == yayinci):
                 rdict = {
-                    "status" : "OK",
+                    "status": "OK",
                     "cmd": microblogistek,
                     "resp": microblogokey,
-                    "count": splitLine[1].strip()
+                    "count": splitLine[ 1 ].strip()
                 }
             else:
                 rdict = {
                     "status": "NOK",
                     "cmd": microblogistek
                 }
-        elif(command == aboneol): #abone olma işlemleri
-            if(type == yayinci):
+        elif (command == aboneol):  # abone olma işlemleri
+            if (type == yayinci):
                 rdict = {
-                    "status" : "OK",
+                    "status": "OK",
                     "cmd": aboneol,
                     "resp": aboneoldun
                 }
@@ -167,10 +168,10 @@ def parser(data, type): #AUTH ve BLCK hataları ana kod içerisinde yazılacak
                     "status": "NOK",
                     "cmd": aboneol
                 }
-        elif(command == aboneliktencik): #abonelikten cikma islemleri
-            if(type == yayinci):
+        elif (command == aboneliktencik):  # abonelikten cikma islemleri
+            if (type == yayinci):
                 rdict = {
-                    "status" : "OK",
+                    "status": "OK",
                     "cmd": aboneliktencik,
                     "resp": aboneliktenciktin
                 }
@@ -179,21 +180,21 @@ def parser(data, type): #AUTH ve BLCK hataları ana kod içerisinde yazılacak
                     "status": "NOK",
                     "cmd": aboneliktencik
                 }
-        elif(command == tweet): #tweet gönderme
-            if(type == yayinci):
+        elif (command == tweet):  # tweet gönderme
+            if (type == yayinci):
                 rdict = {
                     "status": "OK",
                     "cmd": tweet,
                     "resp": tweetokey,
-                    "text": splitLine[1].strip()
+                    "text": splitLine[ 1 ].strip()
                 }
             else:
                 rdict = {
                     "status": "NOK",
                     "cmd": tweet
                 }
-        elif(command == ban): #user ban
-            if(type == yayinci):
+        elif (command == ban):  # user ban
+            if (type == yayinci):
                 rdict = {
                     "status": "OK",
                     "cmd": ban,
@@ -204,8 +205,8 @@ def parser(data, type): #AUTH ve BLCK hataları ana kod içerisinde yazılacak
                     "status": "NOK",
                     "cmd": ban
                 }
-        elif(command == uban): #user unban
-            if(type == yayinci):
+        elif (command == uban):  # user unban
+            if (type == yayinci):
                 rdict = {
                     "status": "OK",
                     "cmd": uban,
@@ -216,13 +217,13 @@ def parser(data, type): #AUTH ve BLCK hataları ana kod içerisinde yazılacak
                     "status": "NOK",
                     "cmd": uban
                 }
-        elif(command == omesaj): #özel mesaj
-            if(type == yayinci):
+        elif (command == omesaj):  # özel mesaj
+            if (type == yayinci):
                 rdict = {
                     "status": "OK",
                     "cmd": omesaj,
                     "resp": mesajok,
-                    "mesaj": splitLine[1].strip()
+                    "mesaj": splitLine[ 1 ].strip()
 
                 }
             else:
@@ -230,130 +231,130 @@ def parser(data, type): #AUTH ve BLCK hataları ana kod içerisinde yazılacak
                     "status": "NOK",
                     "cmd": omesaj
                 }
-        else: #genel hatayı burada tanımlıyoruz
+        else:  # genel hatayı burada tanımlıyoruz
             rdict = {
                 "status": "NOK",
                 "cmd": command,
                 "resp": genelhata
             }
-    except IndexError: #parametre hatası burada tanımlanıyor.
+    except IndexError:  # parametre hatası burada tanımlanıyor.
         rdict = {
             "status": "NOK",
             "cmd": command,
-            "resp":parametre
+            "resp": parametre
         }
 
     return rdict
 
-def inc_parser_server(data, suuid, type, logq, user_dict, flag, clientsenderqueue, clientreaderqueue, public_key, soket):
 
+def inc_parser_server(data, suuid, type, logq, user_dict, clientsenderqueue, clientreaderqueue, public_key, soket):
     if (type == "araci"):
         tip = araci
     else:
         tip = yayinci
-    data_dict = parser(data,tip)
-    data = ""
-    if(data_dict['status'] == "OK"):
-        if(data_dict['cmd'] == merhaba):
-            if(data_dict['cuuid'] in user_dict):
-                data = data_dict['resp2'] + " " + data_dict['cuuid']
-                user_dict[data_dict['cuuid']]['cip'] = data_dict['cip']
-                user_dict[data_dict['cuuid']]['cport'] = data_dict['cport']
-                user_dict[data_dict['cuuid']]['last_login'] = datetime.utcfromtimestamp(time.time()).strftime("%m/%d/%Y %H:%M:%S")
+    c, addr = soket
 
+    data_dict = parser(data, tip)
+    data = ""
+    if (data_dict[ 'status' ] == "OK"):
+        if (data_dict[ 'cmd' ] == merhaba):
+            if (data_dict[ 'cuuid' ] in user_dict):
+                data = data_dict[ 'resp2' ] + " " + data_dict[ 'cuuid' ]
+                user_dict[ data_dict[ 'cuuid' ] ][ 'cip' ] = data_dict[ 'cip' ]
+                user_dict[ data_dict[ 'cuuid' ] ][ 'cport' ] = data_dict[ 'cport' ]
+                user_dict[ data_dict[ 'cuuid' ] ][ 'last_login' ] = time.time()
                 appendToDictionaryFile(user_dict, logq, "araci", "_peer_dictionary.txt")
-                #eğer uuid var ve ip-port farklı ise güncelle sonra welcome dön
-                #gelen_port
-                #gelen uuid
-            else: #WAIT burada göndereliyor ancak ekleme yapmak için client threadinin SUID kontrolünün sonucunu beklemek gerekiyor.
-                data = data_dict['resp1']
+                # eğer uuid var ve ip-port farklı ise güncelle sonra welcome dön
+                # gelen_port
+                # gelen uuid
+            else:  # WAIT burada göndereliyor ancak ekleme yapmak için client threadinin SUID kontrolünün sonucunu beklemek gerekiyor.
+                data = data_dict[ 'resp1' ]
                 command = {
                     'server_flag': "1",
-                    'ip':data_dict['cip'],
-                    'port':data_dict['cport'],
-                    'cmd':"SUID",
-                    'soket':soket,
-                    'data_dict':data_dict
+                    'ip': data_dict[ 'cip' ],
+                    'port': data_dict[ 'cport' ],
+                    'cmd': "SUID",
+                    'soket': soket,
+                    'data_dict': data_dict
                 }
                 print(command)
                 clientsenderqueue.put(command)
-        elif(data_dict['cmd'] == hosgeldin):
-            data = data_dict['resp']
+        elif (data_dict[ 'cmd' ] == hosgeldin):
+            data = data_dict[ 'resp' ]
 
-        elif(data_dict['cmd'] == list):
-
-            if flag == 1:
-#                return_dict = timestamptodate(user_dict)
-                data = data_dict['resp'] + " " + str(user_dict)
+        elif (data_dict[ 'cmd' ] == list):
+            if ispeer_valid(addr[ 0 ], user_dict):
+                #return_dict = timestamptodate(user_dict)
+                data = data_dict[ 'resp' ] + " " + str(user_dict)
             else:
                 data = "AUTH"
 
-        elif (data_dict['cmd'] == suid):
+        elif (data_dict[ 'cmd' ] == suid):
 
-            data = data_dict['resp'] + " " + suuid
+            data = data_dict[ 'resp' ] + " " + suuid
 
-        elif(data_dict['cmd'] == pubkeygeldi):
-            if flag == 1:
-                r_pub_key = data_dict['cpubkey']
+        elif (data_dict[ 'cmd' ] == pubkeygeldi):
+            if ispeer_valid(addr[ 0 ], user_dict):
+                r_pub_key = data_dict[ 'cpubkey' ]
 
                 print("Public key gönderime hazır" + data + "\n")
-                data = data_dict['resp'] + " " + str(public_key)
+                data = data_dict[ 'resp' ] + " " + str(public_key)
 
             else:
                 data = "AUTH"
     else:
-        soket.send(str(data_dict['resp']).encode())
+        soket.send(str(data_dict[ 'resp' ]).encode())
         logq.put(data_dict)
         soket.close()
     return data
 
-def out_parser_client(data, type, my_pub_key, clientSenderQueue, clientReaderQueue):
 
+def out_parser_client(data, type, my_pub_key, clientSenderQueue, clientReaderQueue):
     return 1
 
-def inc_parser_client(data, type, server_dict, fallow_list,pubkey_dict, clientReaderQueue, clientSenderQueue, logq):
 
-    if (data['status'] == "OK"):
-        if (data['cmd'] == listo):
-            list = data["list"]     # Parametre olarak gelen dict alınıyor
-            mergeTwoDict(server_dict, list)     # server_dict'e gelen dict ekleniyor
+def inc_parser_client(data, type, server_dict, fallow_list, pubkey_dict, clientReaderQueue, clientSenderQueue, logq):
+    if (data[ 'status' ] == "OK"):
+        if (data[ 'cmd' ] == listo):
+            list = data[ "list" ]  # Parametre olarak gelen dict alınıyor
+            mergeTwoDict(server_dict, list)  # server_dict'e gelen dict ekleniyor
             log = "Gelen peer listesi asıl listeye eklendi"
             logq.put(log)
             appendToDictionaryFile(server_dict, logq, type, "_peer_dict")
             print("Peerdan alınan liste sözlüğe eklendi\n")
 
-        elif(data['cmd'] == pubkeygeldi):
-            #gelen pub_key i alacak server_dict te ekleyecek fonksiyon
-            cuuid = data['data_dict']['cuuid']
-            pubkey_dict[cuuid] = data['spubkey']
+        elif (data[ 'cmd' ] == pubkeygeldi):
+            # gelen pub_key i alacak server_dict te ekleyecek fonksiyon
+            cuuid = data[ 'data_dict' ][ 'cuuid' ]
+            pubkey_dict[ cuuid ] = data[ 'spubkey' ]
             appendToDictionaryFile(pubkey_dict, logq, type, "_pubkey_dict.txt")
 
-        elif (data['cmd'] == microblogokey):
+        elif (data[ 'cmd' ] == microblogokey):
             pass
-        elif (data['cmd'] == aboneoldun):
-            cuuid = data['data_dict']['cuuid']
+        elif (data[ 'cmd' ] == aboneoldun):
+            cuuid = data[ 'data_dict' ][ 'cuuid' ]
             fallow_list.append(cuuid)
 
             log = "Abonelik işlemi başarıyla gerçekleşti\n"
             logq.put(log)
             print("Abonelik işlemi başarıyla gerçekleşti\n")
 
-        elif (data['cmd'] == aboneliktenciktin):
-            cuuid = data['data_dict']['cuuid']
+        elif (data[ 'cmd' ] == aboneliktenciktin):
+            cuuid = data[ 'data_dict' ][ 'cuuid' ]
             fallow_list.remove(cuuid)
 
             log = "Abonelikten çıkma işlemi başarıyla gerçekleşti\n"
             logq.put(log)
             print("Abonelikten çıkma işlemi başarıyla gerçekleşti\n")
 
-        elif (data['cmd'] == tweetokey):
+        elif (data[ 'cmd' ] == tweetokey):
             pass
-        elif (data['cmd'] == ubanok):
+        elif (data[ 'cmd' ] == ubanok):
             pass
-        elif (data['cmd'] == mesajok):
+        elif (data[ 'cmd' ] == mesajok):
             pass
-        elif (data['cmd'] == authhata):
-            soket = data['server_soket']
+        elif (data[ 'cmd' ] == authhata):
+            soket = data[ 'server_soket' ]
 
             log = "AUTH hatası! Soket kapatıldı.\n"
             logq.put(log)
@@ -361,16 +362,16 @@ def inc_parser_client(data, type, server_dict, fallow_list,pubkey_dict, clientRe
 
             soket.close()
 
-        elif (data['cmd'] == engelhatasi):
-            soket = data['server_soket']
+        elif (data[ 'cmd' ] == engelhatasi):
+            soket = data[ 'server_soket' ]
 
             log = "BLCK hatası! Soket kapatıldı.\n"
             logq.put(log)
             print("BLCK hatası! Soket kapatıldı.\n")
 
             soket.close()
-        elif (data['cmd'] == parametre):
-            soket = data['server_soket']
+        elif (data[ 'cmd' ] == parametre):
+            soket = data[ 'server_soket' ]
 
             log = "ERRP hatası! Soket kapatıldı.\n"
             logq.put(log)
@@ -378,8 +379,8 @@ def inc_parser_client(data, type, server_dict, fallow_list,pubkey_dict, clientRe
 
             soket.close()
 
-        elif (data['cmd'] == genelhata):
-            soket = data['server_soket']
+        elif (data[ 'cmd' ] == genelhata):
+            soket = data[ 'server_soket' ]
 
             log = "ERRG hatası! Soket kapatıldı.\n"
             logq.put(log)
@@ -387,12 +388,4 @@ def inc_parser_client(data, type, server_dict, fallow_list,pubkey_dict, clientRe
 
             soket.close()
 
-
-
     return 1
-
-
-
-
-
-
