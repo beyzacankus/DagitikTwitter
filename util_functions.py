33a63621602d0f1aac4380f99f2f713d24e0f75b
Emre TORAMAN <emretoraman@outlook.com>
2019-01-10 22:47:36 +0300

    minor

--- util_functions.py
+++ util_functions.py
@@ -210,7 +210,7 @@ def ispeer_valid(ip, user_dict):
         return False
 
 def iptouid(ip, user_dict):
-    #user_id = False
+    user_id = False
     for keys in user_dict:
         if user_dict[keys ][ 'cip' ] == str(ip):
             user_id = keys
